e9af52a59f0b55d9186592a2ec3939704f65f92d
Yury <y.kostyuchenko@gmail.com>
2013-04-12 04:55:04 -0700

    Commiting development for task 3

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -8,31 +8,73 @@ class MoviesController < ApplicationController
 
   def index
     @all_ratings = Movie.get_ratings
-    #@selected_ratings = []
-    if params[:ratings] then
-        @selected_ratings = params[:ratings].keys
-        session[:selected_ratings] = @selected_ratings
-    elsif params[:commit] then
-	@selected_ratings = @all_ratings
-	session[:selected_ratings] = []
-    else
-        @selected_ratings = @all_ratings
-	if !session[:selected_ratings] or session[:selected_ratings].length == 0 then
-	    session[:selected_ratings] = []
-	else
-	    @selected_ratings = session[:selected_ratings]
+    current_params, current_session = {}, {}
+    #print "parameters"
+    for par in params.keys do
+	puts par
+	if ["ratings", "sort_header"].count(par.to_s) != 0 then
+	    current_params[par] = params[par]
+	    if session[par] != params[par] then
+		session[par] = params[par]
+	    end
 	end
     end
+    #print "session parameters"
+    for ses in session.keys do
+	#puts ses
+	if ["ratings", "sort_header"].count(ses.to_s) != 0 then
+	   current_session[ses] = session[ses]
+	end
+    end
+    if !session["ratings"] then
+	@selected_ratings = @all_ratings
+    else
+	@rating_chosen = true
+	@selected_ratings = session[:ratings].keys
+    end
+    #@selected_ratings = []
+    #puts params.method_defined?("to_a")
+    #puts session.method_defined?("to_a")
+    #puts Movie.public_instance_methods
+    #if params[:ratings] then
+    #    @selected_ratings = params[:ratings].keys
+    #    session[:ratings] = params[:ratings]
+    #elsif params[:commit] then #after refresh without any checkboxes
+#	@selected_ratings = @all_ratings
+#	session[:ratings] = {}
+#    else
+#        @selected_ratings = @all_ratings
+#	if !session[:ratings] or session[:ratings].length == 0 then
+#	    session[:ratings] = {}
+#	else
+#	    @selected_ratings = session[:ratings].keys
+#	end
+ #   end
     #puts session[:selected_ratings]
-    if !params[:sort_header] then
+    if current_params != current_session then
+	redirect_required = true
+    end
+    #print current_params
+    #print current_session
+    if !session[:sort_header] then
 	#@highlight_title = false
 	#@highlight_release_date = false
         @movies = Movie.where(:rating => @selected_ratings)
     else
 	#puts Movie.public_methods
-	@highlight_title = params[:sort_header] == "title"
-	@highlight_release_date = params[:sort_header] == "release_date"
-	@movies = Movie.order(params[:sort_header]).where(:rating => @selected_ratings)
+	@highlight_title = session[:sort_header] == "title"
+	@highlight_release_date = session[:sort_header] == "release_date"
+	@movies = Movie.order(session[:sort_header]).where(:rating => @selected_ratings)
+    end
+
+    #nparams = params.to_hash.to_a + session[:sort_header] + session[:ratings]
+    #print nparams
+    #nparams = nparams.uniq
+    
+    if redirect_required then
+	flash.keep
+	#print "!!!!!!!!!!!!!!!!!!!!!!"
+	redirect_to movies_path(current_session)
     end
     #end
     #@movies
